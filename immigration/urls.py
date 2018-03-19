374b242830a9a6d28d03670921382d0d35e38113
wueiz <wuezmonster@gmail.com>
2018-03-19 08:52:02 +0800

    feat(init): init the index page

--- immigration/urls.py
+++ immigration/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('index.urls')),
 ]
+
+
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
