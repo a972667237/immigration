38ca6f4862de9caa2e2eec0bc1dad258f55962de
wueiz <wuezmonster@gmail.com>
2018-03-22 20:59:15 +0800

    chore

--- index/models.py
+++ index/models.py
@@ -16,3 +16,19 @@ class Article(models.Model):
         verbose_name_plural = verbose_name
     def __unicode__(self):
         return self.title
+
+
+class Info(models.Model):
+    name = models.CharField(u'名字', max_length=100)
+    phone = models.CharField(u'电话', max_length=11)
+    email = models.EmailField(u'邮箱', max_length=200, null=True)
+    comment = models.TextField(u"评论", max_length=50000)
+    isRead = models.BooleanField(u"是否已读", default=False)
+    create_date = models.DateField(u"创建时间", auto_now_add=True, editable=False)
+
+    class Meta:
+        verbose_name = '用户提交信息'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name + "/" + self.create_date.strftime('%Y-%m-%d') + "/" + (u"已读" if self.isRead else u"未读")
