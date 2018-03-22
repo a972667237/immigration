b3635264ea801f5a1ce13871d4944d8dbb186e8f
wueiz <wuezmonster@gmail.com>
2018-03-22 22:59:16 +0800

    chore

--- index/views.py
+++ index/views.py
@@ -20,6 +20,17 @@ def contact(requests):
 
 
 def article(requests):
-    article_id = requests.GET.get('id')
+    article_id = requests.GET.get('id', 1)
+    try:
+        article_id = int(article_id)
+    except:
+        article_id = 1
+    if article_id == 4:
+        return render(requests, 'australia_knowledge.html', locals())
+    if article_id == 5:
+        return render(requests, 'newzeland_knowledge.html', locals())
+    if article_id == 6:
+        return render(requests, 'canada_knowledge.html', locals())
+
     art = Article.objects.get(pk=article_id)
     return render(requests, 'article.html', locals())
