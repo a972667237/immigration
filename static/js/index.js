a0e2b5892f47234ab532cd06e18d8fff23b379e0
a972667237 <wuezmonster@gmail.com>
2018-03-23 16:11:26 +0800

    chore

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
   new WOW().init();
   $.fatNav();
-  $(".card").hover(function(){
-
-    if($(this).hasClass("pulse")){
-      $(this).removeClass("pulse");
-      $(this).css("animation-name", "none");
-
-    }
-    else{
-      $(this).css("animation-name", "pulse");
-      $(this).addClass("pulse animated");
-    }
-  });
   $(".is-link").click(function() {
       var params1 = $("form").serialize();
       $.ajax( {
